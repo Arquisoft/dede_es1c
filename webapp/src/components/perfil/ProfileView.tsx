e6c9d9a0b3a3640de34071bd08fe8bc2c609ccdb
UO270762 <UO270762@uniovi.es>
2022-03-05 17:41:13 +0100

    Prueba boton material ui

---
+++ webapp/src/components/perfil/ProfileView.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+export const ProfileView = () => {
+    return (
+        <TextField id="outlined-basic" label="Nombre" variant="outlined" />
+    );
+  };
