89a99592fb5aa3dbeb3bd05a499e426cb699b6c1
= <uo239198@uniovi.es>
2022-03-06 16:41:41 +0100

    Añadido el archivo de configuración

---
+++ restapi/config.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+if (process.env.NODE_ENV != 'production') {
+    dotenv.config({path: path.resolve(__dirname, '.env')});
+}
+
+const config = {
+    jwt_secret: process.env.JWT_SECRET  'unsafe_jwt_secret',
+    mongoose: {
+        uri: process.env.MONGODB_URI  'mongodb://localhost/mern'
+    },
+}
+
+export default config;
