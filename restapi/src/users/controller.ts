0dac7c42e4d6dd2300730b70019ec3ff30fb62cf
Alvaro Lopez <varolopez01@gmail.com>
2022-03-11 21:02:55 +0100

    Declaramos metodos para añadir y eliminar al array

--- restapi/src/users/controller.ts
+++ restapi/src/users/controller.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express';
 import {UserModel} from "./model";
+import {ProductModel} from "../products/model";
 
 export default {
     profile: (req: Request, res: Response) => {
@@ -9,5 +10,19 @@ export default {
 
     getAll: async (req: Request, res: Response) => {
         res.json(await UserModel.find());
-    }
+    },
+
+    addProduct: async (req: Request, res: Response) => {
+        const product = await ProductModel.findOne({name: req.params.name});
+
+    },
+
+    deleteProduct: async (req: Request, res: Response) => {
+        if (await ProductModel.findByIdAndDelete(req.params.id)) {
+            res.status(200).json({result: 'OK'});
+        } else {
+            res.status(404).json({error: "The product does not exist"});
+        }
+    },
+
 }
--- restapi/src/users/router.ts
+++ restapi/src/users/router.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import Controller from "./controller";
 import loginRequired from "../middlewares/loginRequired";
+import {ROLES} from "./model";
+import ProductRouter from "../products/router";
 
 const UserRouter = Router()
 
@@ -8,5 +10,7 @@ const UserRouter = Router()
 
 UserRouter.post('/profile', Controller.profile);
 UserRouter.get('/', Controller.getAll);
+ProductRouter.post('/', Controller.addProduct);
+ProductRouter.delete('/:id', Controller.deleteProduct);
 
 export default UserRouter;
