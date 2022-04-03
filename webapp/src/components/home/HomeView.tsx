cbb483a04b059a94c7f71fe198519825b357bdd8
Alvaro Lopez <varolopez01@gmail.com>
2022-04-04 00:05:36 +0200

    Cambiado errores

--- webapp/src/components/home/HomeView.tsx
+++ webapp/src/components/home/HomeView.tsx
@@ -13,35 +13,32 @@ import { ProductCart } from '../../shared/shareddtypes';
 
 
 const useStyle = makeStyles({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    width: "100%",
-  height: "100%",
-  padding: "30px 50px 60px 50px",
-  boxSizing:"border-box",
-  },
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+        padding: "30px 50px 60px 50px",
+        boxSizing:"border-box",
+    },
 });
-type Props = {
-  handleAddToCart: (clickedItem: Product) => void;
-  cartItems: ProductCart[];
-  products:Product[];
-type Props = {
-  
-  handleAddToCart: (clickedItem: Product) => void;
-  cartItems: ProductCart[];
-  products:Product[];}
 
-  export const HomeView:React.FC<Props> = ({ cartItems , handleAddToCart,products})=> {
+type Props = {
+
+    handleAddToCart: (clickedItem: Product) => void;
+    cartItems: ProductCart[];
+    products:Product[];}
+
+export const HomeView:React.FC<Props> = ({ cartItems , handleAddToCart,products})=> {
 
     const classes = useStyle();
     return (
-      <React.Fragment>
-          <div className={classes.container}>
-        <MenuBar cartItems={cartItems}/> 
-        <ProductView props={products} handleAddToCart={handleAddToCart}/>
-        </div>
-        <Icon/>
-      </React.Fragment>
+        <React.Fragment>
+            <div className={classes.container}>
+                <MenuBar cartItems={cartItems}/>
+                <ProductView props={products} handleAddToCart={handleAddToCart}/>
+            </div>
+            <Icon/>
+        </React.Fragment>
     );
-  };
+};
