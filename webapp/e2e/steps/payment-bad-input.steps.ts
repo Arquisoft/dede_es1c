18f4c6675426dfddc32cd4ca08204e1619be47ba
UO270762 <UO270762@uniovi.es>
2022-04-30 13:02:40 +0200

    Test de aceptacion: Pantalla pago datos erroneas

---
+++ webapp/e2e/steps/payment-bad-input.steps.ts
@@ -0,0 +1,50 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import puppeteer from "puppeteer";
+
+const feature = loadFeature('./features/payment-bad-input.feature');
+
+let page: puppeteer.Page;
+let browser: puppeteer.Browser;
+
+defineFeature(feature, test => {
+  
+    jest.setTimeout(100000)
+    beforeAll(async () => {
+  
+      browser = process.env.GITHUB_ACTIONS
+      ? await puppeteer.launch()
+      : await puppeteer.launch({ headless: true, slowMo:100}); //false to run tests locally
+    page = await browser.newPage();
+  
+      await page
+        .goto("http://localhost:3000/Pago", {
+          waitUntil: "networkidle0",
+        })
+        .catch(() => {});
+    });
+    
+    test("The user try to pay with bad card inputs on the form", ({given,when,then}) => {
+  
+        let campoNumeroErroneo:string;
+
+      given("Bad card number", () => {
+          campoNumeroErroneo = "1234"
+      });
+  
+      when("I click in Pagar ", async () => {
+        await page.setViewport({ width: 1200, height: 1300 });
+        await expect(page).toMatch("Pago");
+        await expect(page).toFill("input[name='numero']", campoNumeroErroneo);
+        await expect(page).toMatch("Numero invalido");
+        await expect(page).toClick('button', { text: 'Pagar' });
+        await page.waitForNavigation();
+      });
+  
+      then("A modal popout appear", async () => {
+        await page.waitForNavigation()
+        await page.waitForTimeout(2000);
+        await expect(page).toMatch("Tarjeta invalida, compruebe los campos");
+      });
+    });
+  
+  });
